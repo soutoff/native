e8c799e88c9f9b52f584a97fe16dcc0559d7b2fc
Sergey <i@oforever.ru>
2024-11-17 18:09:34 +0300

    Setting project: tsconfig, prettier

---
+++ client/babel.config.js
@@ -0,0 +1,16 @@
+module.exports = function (api) {
+	api.cache(true)
+	return {
+		presets: ['babel-preset-expo'],
+		// plugins: ["nativewind/babel", "inline-dotenv"]
+		plugins: [
+			// [
+			// 	'babel-plugin-root-import',
+			// 	{
+			// 		rootPathSuffix: 'app/',
+			// 		rootPathPrefix: '/@'
+			// 	}
+			// ]
+		]
+	}
+}
